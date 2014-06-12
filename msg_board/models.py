197ed933be0ab1cf35967c316a995ca104a765a7
Junzki <andrew.junzki@gmail.com>
2014-06-12 10:29:06 +0800

    Update a model

--- msg_board/models.py
+++ msg_board/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 # Create your models here.
 class board_content(models.Model):
-
-#   id = models.AutoField(primary_key=True)
+	
     msg = models.TextField()
-#    from_user = models.CharField(max_length=100, null=true)
     receive_time = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
