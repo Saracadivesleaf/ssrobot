8659a27dcc99ec164a2e1112c40bcb7517d27b70
Junzki <andrew.junzki@gmail.com>
2014-06-23 21:03:05 +0800

    Update Apps & fix some bugs.

--- msg_board/views.py
+++ msg_board/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render_to_response
+from utils.msg_tools import get_msg_body 
+from utils.msg_tools import render_msg_content
 from msg_board.models import messages
 
 
 # Create your views here.
+
+def run(msg_get):
+	from_user = msg_get.FromUserName.text
+	msg_body = get_msg_body(msg_get)
+
+	if msg_body:
+		try:
+			is_written = add_to_board(from_user, msg_body)
+		except:
+			return render_msg_content('text', 'DataBase Error!')
+			
+		if is_written:
+			return render_msg_content('text', 'OK')
+		else:
+			return render_msg_content('text', 'Error!')
+	else:
+		return render_msg_content('text', 'No input')
+
 def add_to_board(from_user, msg_text):
 
 	msg_to_save = messages(openId = from_user, msg = msg_text)
