e33775ab9dca99d06097c5ebb7f5fe98df526de1
Junzki <andrew.junzki@gmail.com>
2014-06-25 08:15:57 +0800

    Add exceptional handling to pkg.

--- pkg/views.py
+++ pkg/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 
 
 from pkg.models import Package
 # Create your views here.
 def get_pkg(key_word):
-	pkg_get = Package.objects.get(key_word = key_word, status = True)
-
-	if pkg_get:
-		return pkg_get.name
-	else:
+	try:
+		pkg_get = Package.objects.get(key_word = key_word, status = True)
+	except ObjectDoesNotExist:
 		return ''
