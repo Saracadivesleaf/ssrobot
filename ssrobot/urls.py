f87de2577803baa711760260bae675f2b54a6222
Junzki <andrew.junzki@gmail.com>
2014-06-26 01:36:59 +0800

    Update admin, using django-xadmin. Update requirements.

--- ssrobot/urls.py
+++ ssrobot/urls.py
@@ -1,18 +1,23 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
+#from django.contrib import admin
+import xadmin
+from xadmin.plugins import xversion
 
 from ssrobot.views import post
 from msg_board.views import show_msg_board
 from checkin.views import show_checkin
-admin.autodiscover()
+#admin.autodiscover()
+
+xadmin.autodiscover()
+xversion.register_models()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ssrobot.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(xadmin.site.urls)),
     url(r'^$', post),
     url(r'^board/', show_msg_board),
     url(r'^checkin', show_checkin),
